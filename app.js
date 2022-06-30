8795cfb078420dc80a03a5d8961d449d4265e0b0
Binh Nguyen <binh.nguyen@credera.co.uk>
2022-06-30 17:52:27 +0100

    feat: first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import SlackBolt from '@slack/bolt';
+
+const { App } = SlackBolt;
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
