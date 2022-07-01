5bbd1342864f521501371c17313d3853b40cedbf
Binh Nguyen <binh.nguyen@credera.co.uk>
2022-07-01 14:51:29 +0100

    feat: trim the text as well

--- src/utils/sanitize.js
+++ src/utils/sanitize.js
@@ -1,3 +1,4 @@
 export default function sanitize(str) {
-  return str.replace(/[^a-zA-Z0-9\s]/g, '');
+  const trimmedText = str.slice(0, 256);
+  return trimmedText.replace(/[^a-zA-Z0-9\s]/g, '');
 }
