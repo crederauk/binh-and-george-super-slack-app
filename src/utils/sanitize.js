c10716021dbc7f98b3c1cd4908b331d1f7fb46a1
Binh Nguyen <binh.nguyen@credera.co.uk>
2022-07-15 08:37:16 +0100

    feat: allow a few more characters re the sanitize function (#5)

--- src/utils/sanitize.js
+++ src/utils/sanitize.js
@@ -1,4 +1,4 @@
 export default function sanitize(str) {
   const trimmedText = str.slice(0, 256);
-  return trimmedText.replace(/[^a-zA-Z0-9\s]/g, '');
+  return trimmedText.replace(/[^a-zA-Z0-9\s\-(),.]/g, '');
 }
